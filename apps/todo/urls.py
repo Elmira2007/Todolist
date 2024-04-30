6c0cfcbd51657af6585794f4ea7746ed1fece417
Elmira2007 <moldosakovaelmira@gmail.com>
2024-04-30 18:12:15 +0600

    models, urls and views done

--- apps/todo/urls.py
+++ apps/todo/urls.py
@@ -1,5 +1,9 @@
+from rest_framework.routers import DefaultRouter
 from django.urls import path
 
-urlpatterns = [
-    
-]
+from apps.todo.views import TodoViewSet
+
+router = DefaultRouter()
+router.register('todo', TodoViewSet, basename='api_todo')
+
+urlpatterns = router.urls
--- apps/todo/views.py
+++ apps/todo/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.urls import path
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 
-# Create your views here.
+from apps.todo.models import Todo
+from apps.todo.serializers import TodoSerializers
+
+class TodoViewSet(GenericViewSet,
+                  mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  mixins.DestroyModelMixin):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializers
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields =['title']
+    permission_classes = (IsAuthenticated, )
